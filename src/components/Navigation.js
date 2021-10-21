acbd5f0ceae4432a67934d91113c9328d452975f
Walter Hernandez <70042323+WalterNights@users.noreply.github.com>
2021-10-20 23:04:06 -0500

    update styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,9 +4,13 @@ import './Navigation.css';
 class Navigation extends Component {
     render(){
         return(
-            <nav className="text-center">
-                <h1 className="nav-title">Axies Calculator V 1.0.0</h1>
-            </nav>
+            <section className="container-nav col-12 d-flex justify-content-center">
+                <nav className="col-8 text-center d-flex justify-content-between align-items-center mb-3">
+                    <p className="">more information soon</p>
+                    <h1 className="nav-title">Axies Calculator V 1.0.0</h1>
+                    <p className="">WalterNights Dev</p>
+                </nav>
+            </section>
         );
     }
 }
