560b41719a14160f4afe6fbd6a84d2833e274482
Walter Hernandez <70042323+WalterNights@users.noreply.github.com>
2021-10-20 22:47:30 -0500

    add buttons for discount energy and cards at once

---
+++ src/AxiesCards.js
@@ -0,0 +1,26 @@
+export const axiesCards = [
+    {
+        "beast": [
+            {
+                "type": "beast",
+                "part": "mouthA",
+                "card": require("./static/axies-cards/beast-cards/mouth/axie_kiss.png")
+            },
+            {
+                "type": "beast",
+                "part": "mouthB",
+                "card": require("./static/axies-cards/beast-cards/mouth/confident.png")
+            },
+            {
+                "type": "beast",
+                "part": "mouthC",
+                "card": require("./static/axies-cards/beast-cards/mouth/goda.png")
+            },
+            {
+                "type": "beast",
+                "part": "mouthD",
+                "card": require("./static/axies-cards/beast-cards/mouth/nut_cracker.png")
+            }
+        ]
+    },
+]
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
+import { axiesCards } from '../AxiesCards';
 
 import './Calculator.css';
 import './AxiesTeam.css'
 
-import Beast from '../static/axie-beast.png';
-import Plant from '../static/axie-plant.png';
-import Bird from '../static/axie-bird.png';
-import Bug from '../static/axie-bug.png';
-import Fish from '../static/axie-fish.png';
-import Reptil from '../static/axie-reptil.png';
-import Meca from '../static/axie-meca.png';
-import Dusck from '../static/axie-dusck.png';
-import Dawn from '../static/axie-dawn.png';
+import Beast from '../static/axies-type/axie-beast.png';
+import Plant from '../static/axies-type/axie-plant.png';
+import Bird from '../static/axies-type/axie-bird.png';
+import Bug from '../static/axies-type/axie-bug.png';
+import Fish from '../static/axies-type/axie-fish.png';
+import Reptil from '../static/axies-type/axie-reptil.png';
+import Meca from '../static/axies-type/axie-meca.png';
+import Dusck from '../static/axies-type/axie-dusck.png';
+import Dawn from '../static/axies-type/axie-dawn.png';
 
 const AxiesList = {
     Bestia: Beast,
@@ -40,7 +41,8 @@ export default class Calculator extends Component {
             cards: 6,
             axieOne: 'Axie Uno',
             axieTwo: 'Axie Dos',
-            axieThree: 'Axie Tres'
+            axieThree: 'Axie Tres',
+            axiesCards
         }
         this.handleEnergyMinus = this.handleEnergyMinus.bind(this)
         this.handleEnergyPlus = this.handleEnergyPlus.bind(this)
@@ -49,8 +51,9 @@ export default class Calculator extends Component {
         this.handleAxieChosen = this.handleAxieChosen.bind(this)
         this.handleCardsMinus = this.handleCardsMinus.bind(this)
         this.handleCardsPlus = this.handleCardsPlus.bind(this)
+        this.handleEnergyAndCardMinus = this.handleEnergyAndCardMinus.bind(this)
     }
-    handleEnergyMinus(e) {
+    handleEnergyMinus() {
         const energy = this.state.energy;
         const energyMinus = this.state.energy - 1
         if(energy > 0){
@@ -59,7 +62,7 @@ export default class Calculator extends Component {
             })
         }
     }
-    handleEnergyPlus(e) {
+    handleEnergyPlus() {
         const energy = this.state.energy;
         const energyPlus = this.state.energy + 1
         if(energy < 10){
@@ -68,7 +71,7 @@ export default class Calculator extends Component {
             })
         }
     }
-    handleCardsMinus(e) {
+    handleCardsMinus() {
         const cards = this.state.cards;
         const cardsMinus = this.state.cards - 1
         if(cards > 0){
@@ -77,12 +80,28 @@ export default class Calculator extends Component {
             })
         }
     }
-    handleCardsPlus(e) {
+    handleCardsPlus() {
         const cards = this.state.cards;
         const cardsPlus = this.state.cards + 1
         if(cards < 32){
             this.setState({
                 cards: + cardsPlus
+            })
+        }
+    }
+    handleEnergyAndCardMinus() {
+        const cards = this.state.cards;
+        const energy = this.state.energy;
+        const cardsMinus = this.state.cards - 1
+        const energyMinus = this.state.energy - 1
+        if(cards > 0){
+            this.setState({
+                cards: + cardsMinus
+            })
+        }
+        if(energy > 0){
+            this.setState({
+                energy: + energyMinus
             })
         }
     }
@@ -144,76 +163,107 @@ export default class Calculator extends Component {
         }
     }
     render() {
+        const axiesCardsLeft = this.state.axiesCards.map((cards, i) => {
+            return (
+                cards.beast.slice(0, 2).map((card, o) => {
+                    return (
+                        <div className="cards-right-container" key={o}>
+                            <img src={card.card.default} alt="img-mouth-beast"></img>
+                        </div>
+                    )
+                })
+            )    
+        });
+        const axiesCardsRight = this.state.axiesCards.map((cards, i) => {
+            return (
+                cards.beast.slice(2, 4).map((card, o) => {
+                    return (
+                        <div className="cards-right-container" key={o}>
+                            <img src={card.card.default} alt="img-mouth-beast"></img>
+                        </div>
+                    )
+                })
+            )    
+        });
         return (
             <div className="col-12 d-flex flex-column justify-content-center align-items-center">
-                <div className="calculator-container">
-                    <div className="card">
-                        <div className="card-header">
-                            <div className="">
-                                <h3>
-                                    Ronda <span className="text-warning">{this.state.round}</span> -
-                                    Energia <span className="text-warning">{this.state.energy}/10</span> -
-                                    Cartas <span className="text-warning">{this.state.cards}</span>
-                                </h3>
+                <div className="calculator-container col-12 d-flex justify-content-evenly align-items-center">
+                    <section className="cards-left-section d-none d-xl-flex justify-content-evenly align-items-center">
+                        {axiesCardsLeft}
+                    </section>
+                    <section className="calculator-section">
+                        <div className="card">
+                            <div className="card-header">
+                                <div className="">
+                                    <h3>
+                                        Ronda <span className="text-warning">{this.state.round}</span> -
+                                        Energia <span className="text-warning">{this.state.energy}/10</span> -
+                                        Cartas <span className="text-warning">{this.state.cards}</span>
+                                    </h3>
+                                </div>
+                            </div>
+                            <div className="card-body">
+                                <div className="d-flex justify-content-around">
+                                    <button className="btn" onClick={this.handleEnergyMinus}>- 1</button>
+                                    <button className="btn" onClick={this.handleEnergyPlus}>+ 1</button>
+                                    <button className="btn" onClick={this.handleNextRound}>Siguiente Ronda</button>
+                                    <button className="btn" onClick={this.handleNewRound}>Nueva Ronda</button>
+                                </div>
+                                <div className="d-flex justify-content-evenly mt-2">
+                                    <button className="btn" onClick={this.handleEnergyAndCardMinus}>- 1 Energia/Carta</button>
+                                    <button className="btn" onClick={this.handleCardsMinus}>- 1 Carta</button>
+                                    <button className="btn" onClick={this.handleCardsPlus}>+ 1 Carta</button>
+                                </div>
+                            </div>
+                            <div className="card-footer">
+                                <div className="mb-3">
+                                    <select name="axieOne" className="form-control" defaultValue={"Axie Uno"} ref={this.sateAxiesOne} onChange={this.handleAxieChosen}>
+                                        <option value="Axie Uno" disabled>Axie Uno</option>
+                                        <option>Bestia</option>
+                                        <option>Dawn</option>
+                                        <option>Dusck</option>
+                                        <option>Insecto</option>
+                                        <option>Meca</option>
+                                        <option>Pajaro</option>
+                                        <option>Pez</option>
+                                        <option>Planta</option>
+                                        <option>Reptil</option>
+                                    </select>
+                                </div>
+                                <div className="mb-3">
+                                    <select name="axieTwo" className="form-control" defaultValue={"Axie Dos"} ref={this.sateAxiesTwo} onChange={this.handleAxieChosen}>
+                                        <option value="Axie Dos" disabled>Axie Dos</option>
+                                        <option value="Bestia">Bestia</option>
+                                        <option value="Dawn">Dawn</option>
+                                        <option value="Dusck">Dusck</option>
+                                        <option value="Insecto">Insecto</option>
+                                        <option value="Meca">Meca</option>
+                                        <option value="Pajaro">Pajaro</option>
+                                        <option value="Pez">Pez</option>
+                                        <option value="Planta">Planta</option>
+                                        <option value="Reptil">Reptil</option>
+                                    </select>
+                                </div>
+                                <div className="mb-3">
+                                    <select name="axieThree" className="form-control" defaultValue={"Axie Tres"} ref={this.sateAxiesThree} onChange={this.handleAxieChosen}>
+                                        <option value="Axie Tres" disabled>Axie Tres</option>
+                                        <option>Bestia</option>
+                                        <option>Dawn</option>
+                                        <option>Dusck</option>
+                                        <option>Insecto</option>
+                                        <option>Meca</option>
+                                        <option>Pajaro</option>
+                                        <option>Pez</option>
+                                        <option>Planta</option>
+                                        <option>Reptil</option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
-                        <div className="card-body">
-                            <div className="d-flex justify-content-around">
-                                <button className="btn" onClick={this.handleEnergyMinus}>- 1</button>
-                                <button className="btn" onClick={this.handleEnergyPlus}>+ 1</button>
-                                <button className="btn" onClick={this.handleNextRound}>Siguiente Ronda</button>
-                                <button className="btn" onClick={this.handleNewRound}>Nueva Ronda</button>
-                            </div>
-                            <div className="d-flex justify-content-evenly mt-2">
-                                <button className="btn" onClick={this.handleCardsMinus}>- 1 Carta</button>
-                                <button className="btn" onClick={this.handleCardsPlus}>+ 1 Carta</button>
-                            </div>
-                        </div>
-                        <div className="card-footer">
-                            <div className="mb-3">
-                                <select name="axieOne" className="form-control" defaultValue={"Axie Uno"} ref={this.sateAxiesOne} onChange={this.handleAxieChosen}>
-                                    <option value="Axie Uno" disabled>Axie Uno</option>
-                                    <option>Bestia</option>
-                                    <option>Dawn</option>
-                                    <option>Dusck</option>
-                                    <option>Insecto</option>
-                                    <option>Meca</option>
-                                    <option>Pajaro</option>
-                                    <option>Pez</option>
-                                    <option>Planta</option>
-                                    <option>Reptil</option>
-                                </select>
-                            </div>
-                            <div className="mb-3">
-                                <select name="axieTwo" className="form-control" defaultValue={"Axie Dos"} ref={this.sateAxiesTwo} onChange={this.handleAxieChosen}>
-                                    <option value="Axie Dos" disabled>Axie Dos</option>
-                                    <option value="Bestia">Bestia</option>
-                                    <option value="Dawn">Dawn</option>
-                                    <option value="Dusck">Dusck</option>
-                                    <option value="Insecto">Insecto</option>
-                                    <option value="Meca">Meca</option>
-                                    <option value="Pajaro">Pajaro</option>
-                                    <option value="Pez">Pez</option>
-                                    <option value="Planta">Planta</option>
-                                    <option value="Reptil">Reptil</option>
-                                </select>
-                            </div>
-                            <div className="mb-3">
-                                <select name="axieThree" className="form-control" defaultValue={"Axie Tres"} ref={this.sateAxiesThree} onChange={this.handleAxieChosen}>
-                                    <option value="Axie Tres" disabled>Axie Tres</option>
-                                    <option>Bestia</option>
-                                    <option>Dawn</option>
-                                    <option>Dusck</option>
-                                    <option>Insecto</option>
-                                    <option>Meca</option>
-                                    <option>Pajaro</option>
-                                    <option>Pez</option>
-                                    <option>Planta</option>
-                                    <option>Reptil</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
+                    </section>
+                    <section className="cards-right-section d-none d-xl-flex justify-content-evenly align-items-center">
+                        {axiesCardsRight}
+                    </section>
                 </div>
                 <div className="container-axies d-xl-flex justify-content-around  mt-5">
                     <div className="card-axies">
