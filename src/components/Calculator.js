54e53b4f28f93823a9ac2ea5cacfaff550827cb0
Walter Hernandez <70042323+WalterNights@users.noreply.github.com>
2021-10-27 14:39:23 -0500

    add titles for section and new option for res cards for the enemy's deck

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,19 +4,21 @@ import './Calculator.css';
 
 import energyIcon from '../static/icons/energy.png'
 import cardIcon from '../static/icons/card_icon.png'
+import deckIcon from '../static/icons/deck_icon.png'
 export default class Calculator extends Component {
     constructor(props) {
         super(props);
-        this.sateAxiesOne = React.createRef();
-        this.sateAxiesTwo = React.createRef();
-        this.sateAxiesThree = React.createRef();
         this.state = {
             round: 1,
             energy: 3,
             cards: 6,
+            deck: 24,
             win: 0,
             lose: 0,
-            percentage: 0
+            percentage: 0,
+            buttonDisableA: false,
+            buttonDisableB: false,
+            buttonDisableC: false
         }
         this.handleEnergyMinus = this.handleEnergyMinus.bind(this)
         this.handleEnergyPlus = this.handleEnergyPlus.bind(this)
@@ -27,6 +29,7 @@ export default class Calculator extends Component {
         this.handleEnergyAndCardMinus = this.handleEnergyAndCardMinus.bind(this)
         this.handleSuccess = this.handleSuccess.bind(this)
         this.handleLose = this.handleLose.bind(this)
+        this.handleRemoveAxie = this.handleRemoveAxie.bind(this)
     }
     handleEnergyMinus() {
         const energy = this.state.energy;
@@ -113,44 +116,44 @@ export default class Calculator extends Component {
         })
     }
     handleNewGame() {
-        const newtRound = 1
-        const newtEnergy = 3
-        const newtCards = 6
-        const newtWin = 0
-        const newtLose = 0
-        const newtPercentage = 0
         this.setState({
-            round: + newtRound,
-            energy: + newtEnergy,
-            cards: + newtCards,
-            win: + newtWin,
-            lose: + newtLose,
-            percentage: + newtPercentage,
+            round: 1,
+            energy: 3,
+            cards: 6,
+            win: 0,
+            lose: 0,
+            deck: 24,
+            percentage: 0,
+            buttonDisableA: false,
+            buttonDisableB: false,
+            buttonDisableC: false
         })
     }
     handleSuccess(e) {
-        const nextRound = 1
-        const nextEnergy = 3
-        const nextCards = 6
         const winRound = this.state.win + 1
         this.setState({
+            round: 1,
+            energy: 3,
+            cards: 6,
+            deck: 24,
             win: + winRound,
-            round: + nextRound,
-            energy: + nextEnergy,
-            cards: + nextCards
+            buttonDisableA: false,
+            buttonDisableB: false,
+            buttonDisableC: false
 
         })
     }
     handleLose(e) {
-        const nextRound = 1
-        const nextEnergy = 3
-        const nextCards = 6
         const loseRound = this.state.lose + 1
         this.setState({
-            round: + nextRound,
-            energy: + nextEnergy,
-            cards: + nextCards,
+            round: 1,
+            energy: 3,
+            cards: 6,
+            deck: 24,
             lose: + loseRound,
+            buttonDisableA: false,
+            buttonDisableB: false,
+            buttonDisableC: false
         })
     }
     componentDidUpdate(prevProps, prevState) {
@@ -158,6 +161,26 @@ export default class Calculator extends Component {
         if (prevState.lose < this.state.lose || prevState.win < this.state.win) {
             this.setState({
                 percentage: + totalPercentage
+            })
+        }
+    }
+
+    handleRemoveAxie(e) {
+        const targetState = "buttonDisable" + e.target.id
+        const deckNumber = this.state.deck - 8
+        if (e.target.id) {
+            if (this.state[targetState] === true) {
+                this.setState({
+                    [targetState]: false
+                })
+            }
+            if (this.state[targetState] === false) {
+                this.setState({
+                    [targetState]: true
+                })
+            }
+            this.setState({
+                deck: deckNumber
             })
         }
     }
@@ -172,7 +195,8 @@ export default class Calculator extends Component {
                                     <h3>
                                         Ronda <span className="text-warning">{this.state.round}</span> -
                                         Energia <img src={energyIcon} alt="energy-icon" /> <span className="text-warning">{this.state.energy}/10</span> -
-                                        Cartas <img src={cardIcon} alt="card-icon" /> <span className="text-warning">{this.state.cards}</span>
+                                        Cartas <img src={cardIcon} alt="card-icon" /> <span className="text-warning">{this.state.cards}</span> /
+                                        Mazo <img src={deckIcon} alt="card-icon" /> <span className="text-warning">{this.state.deck}</span>
                                     </h3>
                                 </div>
                                 <div>
@@ -183,21 +207,36 @@ export default class Calculator extends Component {
                                 </div>
                             </div>
                             <div className="card-body">
-                                <div className="d-flex justify-content-between">
-                                    <button className="btn" onClick={this.handleNewGame}>Nueva Partida</button>
-                                    <button className="btn" onClick={this.handleNextRound}>Siguiente Ronda</button>
-                                    <button className="btn" onClick={this.handleSuccess}>Victoria</button>
-                                    <button className="btn" onClick={this.handleLose}>Derrota</button>
+                                <div className="card-body-content">
+                                    <h3 className="text-warning">Opciones de la Partida</h3>
+                                    <div className="d-flex justify-content-between">
+                                        <button className="btn" onClick={this.handleNewGame}>Nueva Partida</button>
+                                        <button className="btn" onClick={this.handleNextRound}>Siguiente Ronda</button>
+                                        <button className="btn" onClick={this.handleSuccess}>Victoria</button>
+                                        <button className="btn" onClick={this.handleLose}>Derrota</button>
+                                    </div>
                                 </div>
-                                <div className="d-flex justify-content-between mt-2">
-                                    <button className="btn" onClick={this.handleEnergyMinus}>- 1 <img src={energyIcon} alt="energy-icon" /></button>
-                                    <button className="btn" onClick={this.handleEnergyPlus}>+ 1 <img src={energyIcon} alt="energy-icon" /></button>
-                                    <button className="btn" onClick={this.handleEnergyAndCardMinus}>- 1 <img src={energyIcon} alt="energy-icon" />/<img src={cardIcon} alt="card-icon" /></button>
-                                    <button className="btn" onClick={this.handleCardsMinus}>- 1 <img src={cardIcon} alt="card-icon" /></button>
-                                    <button className="btn" onClick={this.handleCardsPlus}>+ 1 <img src={cardIcon} alt="card-icon" /></button>
+                                <div className="card-body-content">
+                                    <h3 className="text-warning">Opciones de Energias y Cartas</h3>
+                                    <div className="d-flex justify-content-between mt-2">
+                                        <button className="btn" onClick={this.handleEnergyMinus}>- 1 <img src={energyIcon} alt="energy-icon" /></button>
+                                        <button className="btn" onClick={this.handleEnergyPlus}>+ 1 <img src={energyIcon} alt="energy-icon" /></button>
+                                        <button className="btn" onClick={this.handleEnergyAndCardMinus}>- 1 <img src={energyIcon} alt="energy-icon" />/<img src={cardIcon} alt="card-icon" /></button>
+                                        <button className="btn" onClick={this.handleCardsMinus}>- 1 <img src={cardIcon} alt="card-icon" /></button>
+                                        <button className="btn" onClick={this.handleCardsPlus}>+ 1 <img src={cardIcon} alt="card-icon" /></button>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="card-footer"></div>
+                            <div className="card-footer">
+                                <div className="card-footer-content">
+                                    <h3 className="text-warning">Equipo Enemigo</h3>
+                                    <div className="d-flex justify-content-around align-items-center mt-2">
+                                        <button id="A" className="btn" onClick={this.handleRemoveAxie} disabled={this.state.buttonDisableA}>Primer Axie</button>
+                                        <button id="B" className="btn" onClick={this.handleRemoveAxie} disabled={this.state.buttonDisableB}>Segundo Axie</button>
+                                        <button id="C" className="btn" onClick={this.handleRemoveAxie} disabled={this.state.buttonDisableC}>Tercer Axie</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </section>
                 </div>
