86fb8ce55d86892bdd311a5ff60930181eb0fd89
Walter Hernandez <70042323+WalterNights@users.noreply.github.com>
2021-10-28 14:31:35 -0500

    updated navbar header

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import './Navigation.css';
+import AppLogo from '../static/icons/axie_calculator_logo.png'
 
 class Navigation extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <section className="container-nav col-12 d-flex justify-content-center">
-                <nav className="col-11 col-xxl-8 text-center d-flex justify-content-between align-items-center mb-md-2">
-                    <p className="">Guías y Ayudas</p>
-                    <h1 className="nav-title">Calculadora Axies V 1.0.0</h1>
-                    <p className="">WalterNights Dev</p>
+                <nav className="col-11 col-xxl-10 d-flex justify-content-center align-items-center text-center">
+                    <div className="col-xxl-4 d-flex justify-content-center align-items-center">
+                        <img src={AppLogo} className="me-2" alt="app-logo" />
+                        <h1 className="nav-title">Axie Calculator</h1>
+                    </div>
+                    <div className="col-xxl-4">
+                        <ul className="d-flex col-12 justify-content-between align-items-center mb-0">
+                            <li><a href="/">Guías y Ayudas</a></li>
+                            <li><a href="/">Contactanos</a></li>
+                            <li><a href="/">Ma información</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-xxl-4">
+                        <p>WalterNightsDev</p>
+                    </div>
                 </nav>
             </section>
         );
