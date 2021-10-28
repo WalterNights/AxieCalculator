93db59c1b68daf0ab612129f718eeb55f2df12ff
Walter Hernandez <70042323+WalterNights@users.noreply.github.com>
2021-10-27 21:58:59 -0500

    delete axieTeam.py, updated styles and add new funtions

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ class Navigation extends Component {
     render(){
         return(
             <section className="container-nav col-12 d-flex justify-content-center">
-                <nav className="col-11 col-xxl-8 text-center d-flex justify-content-between align-items-center mb-3 mb-md-2">
+                <nav className="col-11 col-xxl-8 text-center d-flex justify-content-between align-items-center mb-md-2">
                     <p className="">Guías y Ayudas</p>
                     <h1 className="nav-title">Calculadora Axies V 1.0.0</h1>
                     <p className="">WalterNights Dev</p>
