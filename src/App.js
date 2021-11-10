96de33bdebf425e1c6d92b23e69a253a3266668a
Walter Hernandez <70042323+WalterNights@users.noreply.github.com>
2021-11-10 15:39:58 -0500

    hidden the header nav bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import './App.css';
 
 import Navigation from './components/Navigation'
-//import Calculator from './components/Calculator'
 import CalculatorNew from './components/CalculatorNew'
 
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header d-none">
         <Navigation />
       </header>
       <div className="col-12 d-flex justify-content-center">
-        {/* <Calculator /> */}
         <CalculatorNew/>
       </div>
     </div>
